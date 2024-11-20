af45069f119a175953e8b4daf03d71b718728bdf
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-20 15:10:06 +0100

    change const in let (array of posts) and add to posts.js

--- app.js
+++ app.js
@@ -25,37 +25,3 @@ app.use("/", postsRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// Array dei post
-const posts = [
-  {
-    title: "Ciambellone",
-    content: "Ciambellone ripieno.",
-    image: "/images/ciambellone.jpeg",
-    tags: ["soffice", "ripieno"],
-  },
-  {
-    title: "Cracker Barbabietola",
-    content: "Cracker alla barbabietola.",
-    image: "/images/cracker_barbabietola.jpeg",
-    tags: ["viola", "croccante"],
-  },
-  {
-    title: "Pane Fritto",
-    content: "Pane fritto e dolce.",
-    image: "/images/pane_fritto_dolce.jpeg",
-    tags: ["fritto", "dolce"],
-  },
-  {
-    title: "Pasta Barbabietola",
-    content: "Pasta alla barbabietola.",
-    image: "/images/pasta_barbabietola.jpeg",
-    tags: ["pasta", "barbabietola"],
-  },
-  {
-    title: "Torta Paesana",
-    content: "Torta di latte.",
-    image: "/images/torta_paesana.jpeg",
-    tags: ["latte", "torta"],
-  },
-];
--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+// Array dei post
+let posts = [
+  {
+    title: "Ciambellone",
+    content: "Ciambellone ripieno.",
+    image: "/images/ciambellone.jpeg",
+    tags: ["soffice", "ripieno"],
+  },
+  {
+    title: "Cracker Barbabietola",
+    content: "Cracker alla barbabietola.",
+    image: "/images/cracker_barbabietola.jpeg",
+    tags: ["viola", "croccante"],
+  },
+  {
+    title: "Pane Fritto",
+    content: "Pane fritto e dolce.",
+    image: "/images/pane_fritto_dolce.jpeg",
+    tags: ["fritto", "dolce"],
+  },
+  {
+    title: "Pasta Barbabietola",
+    content: "Pasta alla barbabietola.",
+    image: "/images/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola"],
+  },
+  {
+    title: "Torta Paesana",
+    content: "Torta di latte.",
+    image: "/images/torta_paesana.jpeg",
+    tags: ["latte", "torta"],
+  },
+];
+
 module.exports = router;
