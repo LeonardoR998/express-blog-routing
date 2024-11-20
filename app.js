a3b97094b004b1fa91cada94cff678360a5b41b2
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-20 14:58:00 +0100

    add routers folder

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Rotta base
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+
+// Rotta bacheca
+app.get("/bacheca", (req, res) => {
+  res.json({
+    posts: posts,
+    count: posts.length,
+  });
+});
+
+// Asset statico
+app.use("/images", express.static("public/images"));
+
+const postsRouter = require("./routers/posts");
+
+app.use("/", postsRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 // Array dei post
 const posts = [
   {
@@ -35,23 +59,3 @@ const posts = [
     tags: ["latte", "torta"],
   },
 ];
-
-// Rotta base
-app.get("/", (req, res) => {
-  res.send("Server del mio blog");
-});
-
-// Rotta bacheca
-app.get("/bacheca", (req, res) => {
-  res.json({
-    posts: posts,
-    count: posts.length,
-  });
-});
-
-// Asset statico
-app.use("/images", express.static("public/images"));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
